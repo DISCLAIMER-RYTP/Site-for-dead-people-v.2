fff4238655cb8b8e728751bbe074054f97736db6
vllladik <78494383+vllladik@users.noreply.github.com>
2021-04-19 15:08:10 +0300

    Add-Controllers
    Add controller Car and Employees

---
+++ Ritual Services Api/Controllers/CarController.cs
@@ -0,0 +1,152 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Ritual_Services_Api.Models.Dto;
+using Ritual_Services_Api.Models.Dto.ResultDto;
+using Ritual_Services_Api.Models.Entities;
+using Ritual_Services_Api.Models.Entities.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ritual_Services_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarController : ControllerBase
+    {
+        private readonly ApplicationContext _context;
+
+        public CarController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public CollectionResultDto<CarDto> GetCars()
+        {
+            var cars = _context.Cars.Select(c => new CarDto()
+            {
+                Id = c.Id,
+                Brand = c.Brand,
+                Marks = c.Marks
+            }).ToList();
+
+            return new CollectionResultDto<CarDto>
+            {
+                IsSuccessful = true
+                //Data =
+            };
+        }
+
+        [HttpDelete]
+        public ResultDto DleteCar(int id)
+        {
+            try
+            {
+                if (id != null)
+                {
+                    var c = _context.Cars.Find(id);
+                    _context.Cars.Remove(c);
+                    _context.SaveChanges();
+                    return new ResultDto
+                    {
+                        IsSuccessful = true,
+                        Message = "Sucessfuly deleted"
+                    };
+                }
+                else
+                {
+                    return new ResultDto
+                    {
+                        IsSuccessful = false,
+                        Message = "Id is not defined"
+                    };
+                }
+            }
+            catch (Exception)
+            {
+                return new ResultDto
+                {
+                    IsSuccessful = false,
+                    Message = "Something goes wrong"
+                };
+            }
+        }
+
+        [HttpPost]
+        [Route("Add")]
+        public ResultDto AddCategory([FromBody] CarDto dto)
+        {
+            try
+            {
+                if(dto!=null)
+                {
+                    Car newCar = new Car()
+                    {
+                        Marks = dto.Marks,
+                        Brand = dto.Brand
+                    };
+                    _context.Cars.Add(newCar);
+                    _context.SaveChanges();
+
+                        return new ResultDto
+                        {
+                            IsSuccessful = true,
+                            Message = "Sucessfuly add"
+                        };
+                }
+                else
+                {
+                    return new ResultDto
+                    {
+                        IsSuccessful = false,
+                        Message = "Model is null"
+                    };
+
+                }
+
+            }
+            catch (Exception)
+            {
+                return new ResultDto
+                {
+                    IsSuccessful = false,
+                    Message = "Something goes wrong"
+
+                };
+            }
+        }
+
+        [HttpPost]
+        [Route("Update")]
+        public ResultDto UpdateCar([FromBody] CarDto dto)
+        {
+            try
+            {
+                Car f = _context.Cars.First(x => x.Id == dto.Id);
+
+                f.Brand = dto.Brand;
+                f.Marks = dto.Marks;
+                _context.SaveChanges();
+                return new ResultDto
+                {
+                    IsSuccessful = true,
+                    Message = "Successfully created"
+                };
+            }
+            catch (Exception)
+            {
+                return new ResultDto
+                {
+                    IsSuccessful = false,
+                    Message = "Something goes wrong!"
+                };
+                throw;
+            }
+        }
+
+
+
+    }
+}
---
+++ Ritual Services Api/Controllers/EmployeesController.cs
@@ -0,0 +1,157 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Ritual_Services_Api.Models.Dto;
+using Ritual_Services_Api.Models.Dto.ResultDto;
+using Ritual_Services_Api.Models.Entities;
+using Ritual_Services_Api.Models.Entities.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ritual_Services_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly ApplicationContext _context;
+
+        public EmployeesController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public CollectionResultDto<EmployeesDto>GetEmloyees()
+        {
+            var employeeses = _context.Employees.Select(e => new EmployeesDto()
+            {
+                Id=e.Id,
+                FullName=e.FullName,
+                Position=e.Position,
+                Phone= e.Phone,
+                Image=e.Image,
+                Description=e.Description
+            }).ToList();
+            return new CollectionResultDto<EmployeesDto>
+            {
+                IsSuccessful = true,
+                Data = employeeses
+            };
+        }
+
+        [HttpDelete]
+        public ResultDto DeleteEmployees(int id)
+        {
+            try
+            {
+                if (id != null)
+                {
+                    var e = _context.Employees.Find(id);
+                    _context.Employees.Remove(e);
+                    _context.SaveChanges();
+                    return new ResultDto
+                    {
+                        IsSuccessful = true,
+                        Message = "Successfully deleted"
+                    };
+                }
+                else
+                {
+                    return new ResultDto
+                    {
+                        IsSuccessful = false,
+                        Message = "Id is not defined"
+                    };
+                }
+            }
+            catch (Exception)
+            {
+                return new ResultDto
+                {
+                    IsSuccessful = false,
+                    Message = "Something goes wrong"
+                };
+            }
+        }
+
+
+        [HttpPost]
+        [Route("Add")]
+        public ResultDto AddEmployees([FromBody]EmployeesDto dto)
+        {
+            try
+            {
+                if(dto!=null)
+                {
+                    Employees newEmp = new Employees()
+                    {
+                        FullName = dto.FullName,
+                        Position = dto.Position,
+                        Phone = dto.Phone,
+                        Image = dto.Image,
+                        Description = dto.Description
+                    };
+                    _context.Employees.Add(newEmp);
+                    _context.SaveChanges();
+                    return new ResultDto
+                    {
+                        IsSuccessful = true,
+                        Message = "Sucessfuly add"
+                    };
+
+                }
+                else
+                {
+                    return new ResultDto
+                    {
+                        IsSuccessful = false,
+                        Message = "Model is null"
+                    };
+                }
+            }
+            catch (Exception)
+            {
+                return new ResultDto
+                {
+                    IsSuccessful = false,
+                    Message = "Something goes wrong"
+
+                };
+            }
+        }
+
+        [HttpPost]
+        [Route("Update")]
+        public ResultDto UpdateEmployees([FromBody] EmployeesDto dto)
+        {
+            try
+            {
+                Employees f = _context.Employees.First(x => x.Id == dto.Id);
+
+                f.FullName = dto.FullName;
+                f.Position = dto.Position;
+                f.Phone = dto.Phone;
+                f.Image = dto.Image;
+                f.Description = dto.Description;
+                _context.SaveChanges();
+                return new ResultDto
+                {
+                    IsSuccessful = true,
+                    Message = "Successfully created"
+                };
+            }
+            catch (Exception)
+            {
+                return new ResultDto
+                {
+                    IsSuccessful = false,
+                    Message = "Something goes wrong!"
+                };
+                throw;
+            }
+        }
+
+    }
+}
