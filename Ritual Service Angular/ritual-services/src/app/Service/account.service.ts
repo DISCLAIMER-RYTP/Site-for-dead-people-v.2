0f79ab0af0472c1bc869ba8de4e9c0d57323a997
Anastasia-Troian <56818476+Anastasia-Troian@users.noreply.github.com>
2021-05-11 15:54:40 +0300

    Update Component for NavBar

--- Ritual Service Angular/ritual-services/src/app/Service/account.service.ts
+++ Ritual Service Angular/ritual-services/src/app/Service/account.service.ts
@@ -1,5 +1,5 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ApiResponse, ApiSingleResponse, ResultLoginDto } from '../Models/apiResponse';
 import { RegisterDto } from 'src/app/Models/registerDto';
@@ -10,6 +10,9 @@ import { EditDto, LoginDto } from 'src/app/Models/loginDto';
 })
 export class AccountService {
 
+  headers: HttpHeaders = new HttpHeaders();
+  loginStatus = new EventEmitter<boolean>();
+  
   constructor(private http: HttpClient) { }
   register(user: RegisterDto): Observable<ApiResponse> {
     console.log(user);
@@ -18,7 +21,18 @@ export class AccountService {
   login(user: LoginDto): Observable<ApiResponse> {
     return this.http.post<ApiResponse>('https://localhost:44339/api/account/login', user);
   }
-  
+
+  UploadPhoto(id: number, file: FormData):  Observable<ApiResponse> {
+    this.headers.append('Content-Type', 'multipart/form-data');
+    return this.http.post<ApiResponse>('https://localhost:44339/api/image/UploadImage/'+id, file, {headers: this.headers})
+  }
+
+  LogOut(){
+    localStorage.removeItem("Token");
+    localStorage.removeItem("Id");
+    this.loginStatus.emit(false);
+  }
+
   getUser(id: string):  Observable<ApiResponse> {
     return this.http.get<ApiSingleResponse>('https://localhost:44339/api/account/Profile?id=' + id);
   }
